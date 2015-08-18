fd65f9d874d66d84496da40e69b7e0c7900d929c
Yeo Huang <joke0918@163.com>
2015-08-18 17:06:52 +0800

    no message

---
+++ wishlink/wishlink/Class/Extension/TabBarItem.swift
@@ -0,0 +1,26 @@
+//
+//  TabBarItem.swift
+//  wishlink
+//
+//  Created by Yue Huang on 8/17/15.
+//  Copyright (c) 2015 edonesoft. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UITabBarItem {
+    
+    class func tabBarItem(title: String, image: UIImage, selectedImage:UIImage) -> UITabBarItem{
+        var tabBarItem = UITabBarItem(title: title, image: image.imageWithRenderingMode(.AlwaysOriginal), selectedImage: selectedImage.imageWithRenderingMode(.AlwaysOriginal))
+        NSFontAttributeName
+        let selectedDic = [
+            NSFontAttributeName : UIFont.systemFontOfSize(12),
+            NSForegroundColorAttributeName : UIColor(red: 123 / 255.0, green: 2 / 255.0, blue: 90 / 255.0, alpha: 1.0),
+            
+        ]
+        tabBarItem.setTitleTextAttributes(selectedDic, forState: .Selected)
+        tabBarItem.setTitlePositionAdjustment(UIOffset(horizontal: 0, vertical: -3))
+        return tabBarItem;
+    }
+}
+ 
---
+++ wishlink/wishlink/PageContent/User/U02/U02UserVC.swift
@@ -0,0 +1,43 @@
+//
+//  U02UserVC.swift
+//  wishlink
+//
+//  Created by Yue Huang on 8/17/15.
+//  Copyright (c) 2015 edonesoft. All rights reserved.
+//
+
+import UIKit
+
+class U02UserVC: RootVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil!);
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
