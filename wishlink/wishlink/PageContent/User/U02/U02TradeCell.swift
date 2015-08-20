b75dae9aa34ce35b16c096da05cdfd348300aef5
Yeo Huang <joke0918@163.com>
2015-08-20 23:37:40 +0800

    完善U02UserVC

--- wishlink/wishlink/PageContent/User/U02/U02TradeCell.swift
+++ wishlink/wishlink/PageContent/User/U02/U02TradeCell.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class U02TradeCell: UICollectionViewCell {
 
+    @IBOutlet weak var buyerTopView: UIView!
+    @IBOutlet weak var buyerRoundImageView: UIImageView!
+    @IBOutlet weak var buyerStatusLabel: UILabel!
+    @IBOutlet weak var buyerRevokeBtn: UIButton!
+    @IBOutlet weak var buyerConfirmBtn: UIButton!
+    @IBOutlet weak var buyerCheckComplaintBtn: UIButton!
+    @IBOutlet weak var buyerCheckLogisticsBtn: UIButton!
+    
+    
+    
+    @IBOutlet weak var sellerTopView: UIView!
+    @IBOutlet weak var sellerRoundImageView: UIImageView!
+    @IBOutlet weak var sellerStatusLabel: UILabel!
+    @IBOutlet weak var sellerRevokeBtn: UIButton!
+    @IBOutlet weak var sellerEditItemInfoBtn: UIButton!
+    @IBOutlet weak var sellerCheckComplaintBtn: UIButton!
+    @IBOutlet weak var sellerSendOutBtn: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
