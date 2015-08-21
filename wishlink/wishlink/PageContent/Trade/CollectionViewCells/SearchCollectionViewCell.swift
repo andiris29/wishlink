571d7b7dbcdd9d013e640c6a9dc2284548fb6772
ftchen <274132733@qq.com>
2015-08-22 01:54:06 +0800

    T03Search布局初步

---
+++ wishlink/wishlink/PageContent/Trade/CollectionViewCells/SearchCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCollectionViewCell.swift
+//  wishlink
+//
+//  Created by whj on 15/8/21.
+//  Copyright (c) 2015年 edonesoft. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
