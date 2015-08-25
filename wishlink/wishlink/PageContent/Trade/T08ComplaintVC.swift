02c2bf233d77d04c89194c5c34f85911b6a5378e
ftchen <274132733@qq.com>
2015-08-26 00:08:01 +0800

    T08

--- wishlink/wishlink/PageContent/Trade/T08ComplaintVC.swift
+++ wishlink/wishlink/PageContent/Trade/T08ComplaintVC.swift
@@ -16,10 +16,6 @@ class T08ComplaintVC: RootVC {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
     /*
