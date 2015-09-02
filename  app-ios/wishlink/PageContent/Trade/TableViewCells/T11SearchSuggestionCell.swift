b8177824f2e364531ea8f9bd28c1416955bdfd57
hj_wu <hj_wu@ctrip.com>
2015-09-02 16:00:12 +0800

    T11初步布局

---
+++  app-ios/wishlink/PageContent/Trade/TableViewCells/T11SearchSuggestionCell.swift
@@ -0,0 +1,24 @@
+//
+//  T11SearchSuggestionCell.swift
+//  wishlink
+//
+//  Created by whj on 15/9/2.
+//  Copyright (c) 2015年 edonesoft. All rights reserved.
+//
+
+import UIKit
+
+class T11SearchSuggestionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
