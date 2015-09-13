961f00cc611c553a33e441af21ff8ae74c6bb9fe
wxy325 <wxy325@qq.com>
2015-09-13 22:50:18 +0800

    server-app: fix #37

--- server-segment/wishlink/wishlink/urls.py
+++ server-segment/wishlink/wishlink/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^segmentation/', include('segmentation.urls'))
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^services/segmentation/', include('segmentation.urls'))
 ]
