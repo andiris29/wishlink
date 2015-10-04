f02ae31ad6bbb939675754fcbae81d3aa7c2b517
wxy325 <wxy325@qq.com>
2015-10-04 22:01:41 +0800

    server-app: use mongo full text search in SearchService

--- server-application/lib/model/items.js
+++ server-application/lib/model/items.js
@@ -9,28 +9,38 @@ var entitySchema = Schema({
     status: Number,
     images: [String],
     name: String,
-    nameWords: [String],
+    nameWords: {
+        type : [String]//,
+        //index : 'text'
+    },
     approved: Boolean,
     country: String,
     countryRef: {
         type: Schema.Types.ObjectId,
         ref: 'countries'
     },
-    countryWords: [String],
-
+    countryWords: {
+        type : [String]//,
+        //index : 'text'
+    },
     brand: String,
     brandRef: {
         type: Schema.Types.ObjectId,
         ref: 'brands'
     },
-    brandWords: [String],
-
+    brandWords: {
+        type : [String]//,
+        //index : 'text'
+    },
     category: String,
     categoryRef: {
         type: Schema.Types.ObjectId,
         ref: 'categories'
     },
-    categoryWords: [String],
+    categoryWords: {
+        type : [String]//,
+        //index : 'text'
+    },
     weight: Number,
     spec: String,
     price: Number,
@@ -40,6 +50,25 @@ var entitySchema = Schema({
         'default': Date.now
     }
 });
+
+entitySchema.index(
+    {
+        nameWords: "text",
+        countryWords: "text",
+        brandWords: "text",
+        categoryWords : "text"
+    }
+);
+/*
+db.getCollection('items').createIndex(
+    {
+        nameWords: "text",
+        countryWords: "text",
+        brandWords: "text",
+        categoryWords : "text"
+    }
+)
+*/
 
 var model = mongoose.model('items', entitySchema);
 
