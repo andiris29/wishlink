b59cc10c08c30d43c04ac4f4a4c4a96e59b8e94e
ftchen <274132733@qq.com>
2015-10-08 11:44:01 +0800

    add miss files

--- app-ios/wishlink/Class/DataModel/Trade/tradeModel.swift
+++ app-ios/wishlink/Class/DataModel/Trade/tradeModel.swift
@@ -1,13 +1,42 @@
 //
-//  TradeModel.swift
-//  
+//  tradeModel.swift
+//  wishlink
 //
-//  Created by Yue Huang on 10/7/15.
-//
+//  Created by Andy Chen on 10/7/15.
+//  Copyright (c) 2015 edonesoft. All rights reserved.
 //
 
 import UIKit
 
-class TradeModel: NSObject {
-   
+class tradeModel: BaseModel {
+    
+    var _id:String!
+    
+    var statusOrder:String!
+    var status:Int!
+    var itemRef:String!
+    var ownerRef:String!
+    var create:String!
+    var update:String!
+    
+    var quantity:Int!
+    
+    
+    init(dict:NSDictionary) {
+        super.init()
+        self.tradeModel(dict);
+    }
+    
+    func tradeModel(dict:NSDictionary)
+    {
+        self._id =  self.getStringValue("_id", dic: dict);
+        self.statusOrder =  self.getStringValue("statusOrder", dic: dict);
+        self.status =  self.getIntValue("status", dic: dict);
+        self.itemRef =  self.getStringValue("itemRef", dic: dict);
+        self.ownerRef =  self.getStringValue("ownerRef", dic: dict);
+        self.create =  self.getStringValue("create", dic: dict);
+        self.update =  self.getStringValue("update", dic: dict);
+        self.quantity =  self.getIntValue("quantity", dic: dict);
+        
+    }
 }
