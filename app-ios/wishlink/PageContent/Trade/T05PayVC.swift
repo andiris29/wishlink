f314939a9b2aae9f1eca3d6c1d1d29f20efb1c41
ftchen <274132733@qq.com>
2015-10-13 23:29:52 +0800

    T05原点

--- app-ios/wishlink/PageContent/Trade/T05PayVC.swift
+++ app-ios/wishlink/PageContent/Trade/T05PayVC.swift
@@ -166,7 +166,7 @@ class T05PayVC: RootVC,WebRequestDelegate {
             self.lbReceverMobile.text = defaultAddress.phone;
             self.lbRecevierAddress.text = defaultAddress.address;
             
-            if (item.images == nil) {return}
+            if (item == nil ||  item.images == nil) {return}
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
                 
                 var images: [UIImage] = [UIImage]()
